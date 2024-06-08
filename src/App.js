b2488f91d38e7b8b90f8d5479ca4d6a4dbe54eb2
zakzak25 <zbousba4@gmail.com>
2024-06-08 17:35:25 +0200

    finishing the FCC markdown previewer project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
-function App() {
+const App = () => {
+  const defaultText = `
+# Welcome to my React Markdown Previewer
+## This is a sub-heading
+### Additional things
+
+Some code, \`<div></div>\`, between 2 brackets
+
+\`\`\`javascript
+// This is multiline code
+const myExample = (firstLine, lastLine) => {
+  if (firstLine === '' || lastLine === '') {
+    return 'you are not initialized';
+  }
+}
+\`\`\`
+
+You can also make text **bold**... wow!  
+Or you can make it _italic_ like this.  
+Or bold and italic **_both!_**
+
+There is also a link to [freeCodeCamp](https://www.freecodecamp.org).
+
+> Block Quotes
+
+![React Logo with Text](https://brandslogos.com/wp-content/uploads/images/react-logo-1.png)
+
+- And last but not least, there's a list:
+  - I am a list item
+    - With different indentation
+      - That looks like this
+`;
+
+  const [writtenText, setWrittenText] = useState(defaultText);
+
+  const changeHandler = (event) => {
+    setWrittenText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* EDITOR */}
+      <div className="editor">
+        <div className="title">Editor</div>
+        <textarea
+          id="editor"
+          value={writtenText}
+          onChange={changeHandler}
+        />
+      </div>
+
+      {/* PREVIEWER */}
+      <div className="previewer">
+        <div className="title">Previewer</div>
+        <div id='preview'>
+        <ReactMarkdown>{writtenText}</ReactMarkdown>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
